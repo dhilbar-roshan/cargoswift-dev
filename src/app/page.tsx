99542deb11ac6144a27a6310ab09eb42556c3891
sreeragh-s <sreeragh.s@icloud.com>
2023-11-27 07:29:55 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div className="flex items-center justify-center h-screen">
-    <div className="bg-black h-10 text-center text-white">Login</div>
+    <a href='/login' className="bg-black p-3 rounded-md text-center text-white">Login</a>
 </div>
 
   )
