96ec7e34678980e2ea9c900583a32c7833d5ca3a
sreeragh-s <sreeragh.s@icloud.com>
2023-11-27 10:29:40 +0530

    commit

--- src/app/onboard/page.tsx
+++ src/app/onboard/page.tsx
@@ -13,7 +13,7 @@ export default function page() {
   return (
   
 
-<nav className="bg-white border-gray-200 ">
+<nav className="bg-white h-screen ">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="/logo.png" className="h-8" alt="Flowbite Logo" />
@@ -25,13 +25,7 @@ export default function page() {
         <img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo"/>
       </button>
       {/* <!-- Dropdown menu --> */}
-      <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow " id="user-dropdown">
-        <div className="px-4 py-3">
-          <span className="block text-sm text-gray-900 ">Name</span>
-          <span className="block text-sm  text-gray-500 truncate ">name@email.com</span>
-        </div>
-  
-      </div>
+     
 
   </div>
 
@@ -47,114 +41,65 @@ export default function page() {
 </div>
 
 
-<div className='mt-10 mx-2'>
+
+
+
+<div>
+<Drawer.Root>
+<div className='my-10 mx-2'>
 <Iframe width="100%" height="500"  url="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=+(tokyo)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></Iframe>
 </div>
-
-
-
-<Drawer.Root>
-      <Drawer.Trigger asChild>
-        <div className='bg-black text-center mt-10 p-5 text-white mx-2 rounded-md'>Next</div>
+      <Drawer.Trigger asChild>  
+        <button className='w-full bg-black text-center mb-20 p-5 text-white mx-2 rounded-md'>Next</button>
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0">
-          <div className="p-4 bg-white rounded-t-[10px] flex-1">
-            <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
+          <div className="p-4  mb-16 bg-white rounded-t-[10px] flex-1">
+            <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 " />
             <div className="max-w-md mx-auto">
               <Drawer.Title className="font-medium mb-4">
                 Select Weight
               </Drawer.Title>
-              <p className="text-zinc-600 mb-2">
+              <p className="text-zinc-600">
                 This component can be used as a replacement for a Dialog on
                 mobile and tablet devices.
               </p>
-        
             </div>
           </div>
-          <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
-            <div className="flex gap-6 justify-end max-w-md mx-auto">
-              <a
-                className="text-xs text-zinc-600 flex items-center gap-0.25"
-                href="https://github.com/emilkowalski/vaul"
-                target="_blank"
-              >
-                GitHub
-                <svg
-                  fill="none"
-                  height="16"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  width="16"
-                  aria-hidden="true"
-                  className="w-3 h-3 ml-1"
-                >
-                  <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
-                  <path d="M15 3h6v6"></path>
-                  <path d="M10 14L21 3"></path>
-                </svg>
-              </a>
-              <a
-                className="text-xs text-zinc-600 flex items-center gap-0.25"
-                href="https://twitter.com/emilkowalski_"
-                target="_blank"
-              >
-                Twitter
-                <svg
-                  fill="none"
-                  height="16"
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  viewBox="0 0 24 24"
-                  width="16"
-                  aria-hidden="true"
-                  className="w-3 h-3 ml-1"
-                >
-                  <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
-                  <path d="M15 3h6v6"></path>
-                  <path d="M10 14L21 3"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
+       
         </Drawer.Content>
       </Drawer.Portal>
     </Drawer.Root>
 
-
+    </div>
 
 <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 ">
     <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
-        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 ">
-            <svg className="w-5 h-5 mb-2 text-black  group-hover:text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <button type="button" className="inline-flex flex-col items-center justify-center px-5  ">
+            <svg className="w-5 h-5 mb-2 text-black  group-hover:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
             </svg>
-            <span className="text-sm text-gray-500 group-hover:text-blue-600">Home</span>
+            <span className="text-sm text-gray-500 group-hover:text-black">Home</span>
         </button>
-        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 ">
-            <svg className="w-5 h-5 mb-2 text-gray-500 group-hover:text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <button type="button" className="inline-flex flex-col items-center justify-center px-5  ">
+            <svg className="w-5 h-5 mb-2 text-gray-500 group-hover:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M11.074 4 8.442.408A.95.95 0 0 0 7.014.254L2.926 4h8.148ZM9 13v-1a4 4 0 0 1 4-4h6V6a1 1 0 0 0-1-1H1a1 1 0 0 0-1 1v13a1 1 0 0 0 1 1h17a1 1 0 0 0 1-1v-2h-6a4 4 0 0 1-4-4Z"/>
                 <path d="M19 10h-6a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1Zm-4.5 3.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2ZM12.62 4h2.78L12.539.41a1.086 1.086 0 1 0-1.7 1.352L12.62 4Z"/>
             </svg>
-            <span className="text-sm text-gray-500 group-hover:text-blue-600">Wallet</span>
+            <span className="text-sm text-gray-500 group-hover:text-black">Wallet</span>
         </button>
-        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 ">
-            <svg className="w-5 h-5 mb-2 text-gray-500 group-hover:text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <button type="button" className="inline-flex flex-col items-center justify-center px-5  ">
+            <svg className="w-5 h-5 mb-2 text-gray-500 group-hover:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
             </svg>
-            <span className="text-sm text-gray-500 group-hover:text-blue-600">Settings</span>
+            <span className="text-sm text-gray-500 group-hover:text-black">Settings</span>
         </button>
-        <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 ">
-            <svg className="w-5 h-5 mb-2 text-gray-500 group-hover:text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <button type="button" className="inline-flex flex-col items-center justify-center px-5  ">
+            <svg className="w-5 h-5 mb-2 text-gray-500 group-hover:text-black" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
             </svg>
-            <span className="text-sm text-gray-500 group-hover:text-blue-600">Profile</span>
+            <span className="text-sm text-gray-500 group-hover:text-black">Profile</span>
         </button>
     </div>
 </div>
